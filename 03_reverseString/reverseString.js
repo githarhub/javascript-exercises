7502800cd045f4e7c04512478782d0505d369fe6
githarhub <179049148+githarhub@users.noreply.github.com>
2024-10-26 04:30:32 +0630

    feat: solve 03_reverseString
    added argument text and default value to ""
    change string to array and reverse array and join array with no separator
    change reversed and joined array to string and return

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(text = "") {
 
+    const notReverse = text.split("");
+    const reverse = notReverse.reverse().join("");
+    return reverse.toString();
 };
 
 // Do not edit below this line
