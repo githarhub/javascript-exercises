aa5673d5f4d7f906ea15a9272bf56babb9851110
githarhub <179049148+githarhub@users.noreply.github.com>
2024-10-26 04:13:56 +0630

    feat : solve last two of 02_repeatString
    added condition to check if times is null if then generate random number
    last part is already solve by using empty string to return value

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
 const repeatString = function(text, times) {
     let repeatText = "";
-    if (times < 0) {
+    if (times === null) {
+        times = Math.floor(Math.random() * 1000);
+    }
+    else if (times < 0) {
         return "ERROR";
     }
     for (let i = 0; i < times; i++) {
