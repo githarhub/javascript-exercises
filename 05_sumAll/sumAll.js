57e098f341ff15165472c17004f2449610ef1188
githarhub <179049148+githarhub@users.noreply.github.com>
2024-10-27 02:24:33 +0630

    feat: solve last five of 05_sumAll
    created validNumber function that check if variable are vaild
    if isn't valid return "ERROR"
    check if number are small to big if isn't swap them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function(startNumber, endNumber) {
-    return sumNumber(startNumber, endNumber)
+
+    if (validNumber(startNumber, endNumber) === false) {
+        return "ERROR";
+    }
+    else if (endNumber < startNumber) {
+        let bufferNumber = startNumber;
+        startNumber = endNumber;
+        endNumber = bufferNumber;
+    };
+    return sumNumber(startNumber, endNumber);
 
 };
 
@@ -11,6 +20,18 @@ function sumNumber(startNumber, endNumber) {
 
     return summedNumber;
 } 
+
+function validNumber (firstInput, secondInput) {
+    if (Number.isInteger(firstInput) && Number.isInteger(secondInput)) {
+        if (firstInput < 0 || secondInput < 0) {
+            return false;
+        }
+        return true;
+    };
+
+    return false;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
