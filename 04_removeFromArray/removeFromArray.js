7c335d8f4ce0b40b3e4c867d941dcf1d31c1152f
githarhub <179049148+githarhub@users.noreply.github.com>
2024-10-26 22:26:37 +0630

    feat: solve 04_removeFromArray
    added inputArray, ...removedNumber to function removeFromArray
    created removedArray and filter
    created checkNumber function to check removeNumber

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removeNumber) {
+    
+    const removedArray = inputArray.filter(currentNumber => checkNumber(currentNumber, removeNumber));
+    return removedArray;
 };
-
+function checkNumber(inputNumber, removeNumber) {
+    for (const number of removeNumber) {
+        if (inputNumber === number) {
+            return false;
+        };
+    };
+    return true;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
