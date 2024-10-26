bb19f1e9bbe9f6c3a685166119d84a212f717928
githarhub <179049148+githarhub@users.noreply.github.com>
2024-10-26 22:38:06 +0630

    feat: solve first two of 04_sumAll
    added startNumber and endNumber to function sumAll
    created sumNumber function that take two parameters and sum number between two parameters and themselves

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    return sumNumber(startNumber, endNumber)
 
 };
+
+function sumNumber(startNumber, endNumber) {
+    let summedNumber = 0;
+    for(let i = startNumber; i <= endNumber; i++) {
+        summedNumber += i;
+    }
+
+    return summedNumber;
+} 
 
 // Do not edit below this line
 module.exports = sumAll;
