de6788eb6561667c70909bd5616cf56221db9413
githarhub <179049148+githarhub@users.noreply.github.com>
2024-10-27 03:43:07 +0630

    feat: solve all of 07_tempConversion
    created roundedDecimal function that round one decimal place
    created fahrenheit argument for convertToCelsius and calculate and round using roundedDecimal
    created celsius argument for convertToFahrenheit calculate and and round using roundedDecimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const unroundedCelsius = (fahrenheit - 32) * (5/9);
+  const roundedCelsius = roundDecimal(unroundedCelsius);
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const unroundedFahrenheit = ((celsius * (9/5)) + 32);
+  const roundedFahrenheit = roundDecimal(unroundedFahrenheit);
+  return roundedFahrenheit;
+};
+
+
+function roundDecimal(decimal){
+   const roundedIntegerNumber = Math.round(decimal * 10);
+   const roundedDecimalNumber = roundedIntegerNumber / 10;
+   return roundedDecimalNumber;
 };
 
 // Do not edit below this line
