5dd746741b9f96f1dc84df5175799f006a84bba0
githarhub <179049148+githarhub@users.noreply.github.com>
2024-12-04 03:54:26 +0630

    feat: solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+    const onlyChar = text.toLowerCase().replaceAll(/[^a-z0-9]/g, "").split("");
+
+    const reversedOnlyChar = onlyChar.slice().reverse();
+
+
+    return onlyChar.every( (element, index)  => {
+
+        return element === reversedOnlyChar[index]
+
+    })
 
 };
 
