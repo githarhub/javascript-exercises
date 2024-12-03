9171d5d492e29594cf301320c629da2351133106
githarhub <179049148+githarhub@users.noreply.github.com>
2024-12-04 04:52:25 +0630

    feat: solve findTheOldest
    solved all of javascript exercises in repo

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+
+
+    return people.reduce((oldest, person) => {
+
+        if (!person.hasOwnProperty("yearOfDeath")) {
+
+            person.yearOfDeath = 2024;
+        }
+
+        if (oldest === 0) {
+            return person;
+        }
+        
+        oldAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        personAge = person.yearOfDeath - person.yearOfBirth;
+
+        return (oldAge > personAge) ? oldest : person;
+    }, 0)
 };
 
 // Do not edit below this line
