515a2057ad60c3dcdba9fab1756f86c120bd9ffe
githarhub <179049148+githarhub@users.noreply.github.com>
2024-12-04 04:27:34 +0630

    feat: solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(number) { 
+
+    number = parseInt(number);
+
+    if (number < 0) {
+        return "OOPS"
+    }
+
+    if (number == 0) {
+        return 0;
+    }
+
+    
+    let total = 1;
+    let firstNumber = 1;
+    let secondNumber = 0;
+
+    for (let i = 1; i < number; i++) {
+        total = firstNumber + secondNumber;
+        buffer = firstNumber;
+        firstNumber = total;
+        secondNumber = buffer;
+    }
+
+    return total;
 
 };
 
